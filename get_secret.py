9c51f93aecbf18244ed869466072636642ea7354
UTXOnly <brian.hartford@datadoghq.com>
2025-05-20 12:15:08 -0400

    Reformat output

--- get_secret.py
+++ get_secret.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-
-import requests
+import json
 import sys
+import requests
 
 # Configuration
-vault_uri = "https://brian-hartford-key-vault.vault.azure.net"
-secret_name = "brian-hartford-secret"
-api_version = "7.3"
+VAULT_URI = "https://brian-hartford-key-vault.vault.azure.net"
+API_VERSION = "7.3"
 
 def get_managed_identity_token(resource="https://vault.azure.net"):
     """Fetch an access token from Azure Managed Identity endpoint."""
@@ -23,26 +22,49 @@ def get_managed_identity_token(resource="https://vault.azure.net"):
         response.raise_for_status()
         return response.json()["access_token"]
     except Exception as e:
-        print(f"Failed to get access token from Managed Identity:\n{e}")
+        print(f"Failed to get access token from Managed Identity: {e}")
         sys.exit(1)
 
-def get_secret(token):
+def fetch_secret_from_azure(token, secret_name):
     """Retrieve a secret from Azure Key Vault using the provided token."""
     try:
-        url = f"{vault_uri}/secrets/{secret_name}?api-version={api_version}"
+        url = f"{VAULT_URI}/secrets/{secret_name}?api-version={API_VERSION}"
         headers = {
             "Authorization": f"Bearer {token}"
         }
         response = requests.get(url, headers=headers)
         response.raise_for_status()
-        secret = response.json()
-        print("Secret retrieved successfully:")
-        print(secret["value"])
+        return response.json()["value"]
     except Exception as e:
-        print(f"Failed to retrieve secret:\n{e}")
-        print(f"Response: {response.text}")
+        return None
+
+def retrieve_secrets():
+    try:
+        secret_request = json.load(sys.stdin)
+        token = get_managed_identity_token()
+        
+        secret_response = {}
+        
+        for secret in secret_request["secrets"]:
+            secret_value = fetch_secret_from_azure(token, secret)
+            if secret_value is not None:
+                secret_response[secret] = {
+                    "value": str(secret_value),
+                    "error": None
+                }
+            else:
+                secret_response[secret] = {
+                    "value": None,
+                    "error": "Unable to retrieve secret."
+                }
+                
+    except Exception as e:
+        print(f"There was an error retrieving secrets: {e}")
         sys.exit(1)
+        
+    sys.stdin.close()
+    return secret_response
 
 if __name__ == "__main__":
-    token = get_managed_identity_token()
-    get_secret(token)
+    secrets = retrieve_secrets()
+    print(json.dumps(secrets))
